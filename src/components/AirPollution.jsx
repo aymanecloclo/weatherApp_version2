d66f1c8d88b2652b35031cdbcf94c2315e55f5a7
aymane <aymanerachid47@gmail.com>
2024-11-17 15:12:00 +0100

    fixed responsive issue

---
+++ src/components/AirPollution.jsx
@@ -0,0 +1,62 @@
+// src/AirPollution.js
+
+import React, { useEffect, useState } from 'react';
+import { FaWind, FaSmog, FaCloudSunRain } from 'react-icons/fa'; // Ajout d'icônes
+
+const AirPollution = () => {
+    const lat = 34.020882; // Latitude for Rabat
+    const lon = -6.841650;
+    const [pollutionData, setPollutionData] = useState(null);
+    const apiKey = '7ab4338a56030b82d25ba8a78b578696'; // Remplacez par votre clé API OpenWeatherMap
+
+    useEffect(() => {
+        const fetchAirPollutionData = async () => {
+            try {
+                const response = await fetch(`http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${apiKey}`);
+                const data = await response.json();
+                setPollutionData(data);
+            } catch (error) {
+                console.error('Error fetching air pollution data:', error);
+            }
+        };
+
+        fetchAirPollutionData();
+    }, [lat, lon, apiKey]);
+
+    return (
+        <div className=" w-full flex justify-center  md:min-h-screen py-10">
+            <div className="bg-white shadow-2xl rounded-3xl p-8 max-w-lg w-full transition-transform duration-300 transform hover:scale-105">
+                <h2 className="text-4xl font-extrabold text-center text-blue-800 mb-6">
+                    Pollution de l'Air
+                </h2>
+                <div className="flex justify-between items-center mb-4">
+                    <FaWind className="text-blue-600 text-3xl" />
+                    <h3 className="text-2xl font-semibold text-blue-800">AQI: 
+                        <span className={`ml-2 font-bold ${pollutionData && pollutionData.list[0].main.aqi === 1 ? 'text-green-500' : pollutionData && pollutionData.list[0].main.aqi === 2 ? 'text-yellow-500' : pollutionData && pollutionData.list[0].main.aqi === 3 ? 'text-orange-500' : pollutionData && pollutionData.list[0].main.aqi === 4 ? 'text-red-500' : 'text-purple-500'}`}>
+                            {pollutionData ? pollutionData.list[0].main.aqi : 'N/A'}
+                        </span>
+                    </h3>
+                    <FaSmog className="text-blue-600 text-3xl" />
+                </div>
+                <h4 className="text-xl font-semibold mb-4 text-blue-800">Poolluants Principaux:</h4>
+                <ul className="list-disc pl-5 space-y-2">
+                    {pollutionData ? (
+                        Object.entries(pollutionData.list[0].components).map(([key, value]) => (
+                            <li key={key} className="flex justify-between text-blue-600">
+                                <span className="capitalize">{key}:</span>
+                                <span>{value} µg/m³</span>
+                            </li>
+                        ))
+                    ) : (
+                        <li className="text-gray-600">Chargement...</li>
+                    )}
+                </ul>
+                <div className="mt-6 flex justify-center">
+                    <FaCloudSunRain className="text-blue-600 text-4xl animate-bounce" />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default AirPollution;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,66 @@
+<footer>
+    <div class="bg-gray-800 py-4 text-gray-400">
+      <div class="container px-4 mx-auto">
+        <div class="-mx-4 flex flex-wrap justify-between">
+          <!-- Logo and Description -->
+          <div class="px-4 my-4 w-full xl:w-1/5">
+            <a href="/" class="block w-56 mb-10">
+              <!-- Your logo SVG here -->
+            </a>
+            <p class="text-justify">
+              Tailwindow is a collection of UI Components created using Tailwind CSS Framework...
+            </p>
+          </div>
+
+          <!-- Company Links -->
+          <div class="px-4 my-4 w-full sm:w-auto">
+            <div>
+              <h2 class="inline-block text-2xl pb-4 mb-4 border-b-4 border-blue-600">Company</h2>
+            </div>
+            <ul class="leading-8">
+              <li><a href="#" class="hover:text-blue-400">About Us</a></li>
+              <li><a href="#" class="hover:text-blue-400">Terms &amp; Conditions</a></li>
+              <li><a href="#" class="hover:text-blue-400">Privacy Policy</a></li>
+              <li><a href="#" class="hover:text-blue-400">Contact Us</a></li>
+            </ul>
+          </div>
+
+          <!-- Blog Links -->
+          <div class="px-4 my-4 w-full sm:w-auto">
+            <div>
+              <h2 class="inline-block text-2xl pb-4 mb-4 border-b-4 border-blue-600">Blog</h2>
+            </div>
+            <ul class="leading-8">
+              <li><a href="#" class="hover:text-blue-400">Getting Started With HTML and CSS</a></li>
+              <li><a href="#" class="hover:text-blue-400">What Is Flex And When to Use It?</a></li>
+              <li><a href="#" class="hover:text-blue-400">How TailwindCSS Can Help Your Productivity?</a></li>
+              <li><a href="#" class="hover:text-blue-400">5 Tips to Make Responsive Website</a></li>
+              <li><a href="#" class="hover:text-blue-400">See More</a></li>
+            </ul>
+          </div>
+
+          <!-- Social Media Links -->
+          <div class="px-4 my-4 w-full sm:w-auto xl:w-1/5">
+            <div>
+              <h2 class="inline-block text-2xl pb-4 mb-4 border-b-4 border-blue-600">Connect With Us</h2>
+            </div>
+            <!-- Social media icons -->
+            <div class="flex space-x-3">
+              <a href="#" class="inline-flex items-center justify-center h-8 w-8 border border-gray-100 rounded-full mr-1 hover:text-blue-400 hover:border-blue-400">
+                <!-- Facebook Icon SVG -->
+              </a>
+              <a href="#" class="inline-flex items-center justify-center h-8 w-8 border border-gray-100 rounded-full mr-1 hover:text-blue-400 hover:border-blue-400">
+                <!-- Twitter Icon SVG -->
+              </a>
+              <a href="#" class="inline-flex items-center justify-center h-8 w-8 border border-gray-100 rounded-full mr-1 hover:text-blue-400 hover:border-blue-400">
+                <!-- LinkedIn Icon SVG -->
+              </a>
+              <a href="#" class="inline-flex items-center justify-center h-8 w-8 border border-gray-100 rounded-full mr-1 hover:text-blue-400 hover:border-blue-400">
+                <!-- Instagram Icon SVG -->
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+</footer>
